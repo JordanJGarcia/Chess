db19784953add72a8de33a732a4e79640adb52d0
Jordan Garcia <j.joseph_g@yahoo.com>
2024-01-01 13:09:59 -0500

    Add requestMove() method

--- Player.java
+++ Player.java
@@ -5,8 +5,9 @@ import java.awt.*;
 
 class Player {
 
-    private Orientation orientation;
+    private Orientation or;
     private Piece piece[] = new Piece[16];
+    private BitBoard bb; // each player has a copy of the bb that gets updated
 
 
     // default constructor 
@@ -15,8 +16,9 @@ class Player {
     }
 
     // constructor
-    Player(Orientation o) {
+    Player(Orientation o, BitBoard b) {
         setOrientation(o);
+        setBitBoard(b);
         setPlayerPieces();
     }
 
@@ -25,70 +27,116 @@ class Player {
     /*--------------------------------------*/
 
     Orientation getOrientation() {
-        return this.orientation;
+        return or;
     }
 
     void setOrientation(Orientation o) {
-        this.orientation = o;
+        or = o;
+    }
+
+    BitBoard getBitBoard() {
+        return bb;
+    }
+
+    void setBitBoard(BitBoard b) {
+        bb = b;
     }
 
     void setPlayerPieces() {
         int i, j;
 
-        if(this.getOrientation() == Orientation.WHITE)
+        if(getOrientation() == Orientation.WHITE)
             i = 48;
         else
             i = 8;
 
         // place pawns on second row
         for(j = 0; j < 8; j++, i++)
-            piece[j] = new Pawn(i, this.getOrientation());
+            piece[j] = new Pawn(i, getOrientation());
 
         // if black, non-pawn pieces get placed behind
-        if(this.getOrientation() == Orientation.BLACK)
+        if(getOrientation() == Orientation.BLACK)
             i = 0;
 
         // place non-pawn pieces
-        piece[j++] = new Rook(i++, this.getOrientation());
-        piece[j++] = new Knight(i++, this.getOrientation());
-        piece[j++] = new Bishop(i++, this.getOrientation());
-        piece[j++] = new Queen(i++, this.getOrientation());
-        piece[j++] = new King(i++, this.getOrientation());
-        piece[j++] = new Bishop(i++, this.getOrientation());
-        piece[j++] = new Knight(i++, this.getOrientation());
-        piece[j++] = new Rook(i++, this.getOrientation());
+        piece[j++] = new Rook(i++, getOrientation());
+        piece[j++] = new Knight(i++, getOrientation());
+        piece[j++] = new Bishop(i++, getOrientation());
+        piece[j++] = new Queen(i++, getOrientation());
+        piece[j++] = new King(i++, getOrientation());
+        piece[j++] = new Bishop(i++, getOrientation());
+        piece[j++] = new Knight(i++, getOrientation());
+        piece[j++] = new Rook(i++, getOrientation());
     }
 
     @Override public String toString() {
-        return("Player: " + this.getOrientation());
+        return("Player: " + getOrientation());
     }
 
     void printPlayerData() {
-        System.out.println(this.toString());
-        for(Piece p : this.piece)
+        System.out.println(toString());
+        for(Piece p : piece)
             System.out.print(p.toString());
+
         System.out.println("");
+        System.out.println("player " + or + " bitboard: ");
+        bb.printBitBoard();
     }
 
     /*--------------------------------------*/
     /* methods to mimic player capabilities */
     /*--------------------------------------*/
 
-    int requestMove(Piece p, int loc) {
-        // maybe generate an attack mask for this piece?
-        // also generate an open move mask
-        // validate player can make move based on piece type
-        // make move or return error (-1)
-        return -1;
+    int requestMove(int from, int to) {
+        long validMoves = 0L;
+        Piece p = null;
+
+        for(Piece t : piece) {
+            if(t.getPosition() == from)
+                p = t;
+        }
+
+        if(p == null)
+            return -1;
+
+        switch(p.getType()) {
+            case PAWN:
+                validMoves = bb.getPawnMoves(from, or, p.getNumMoves() == 0);
+                break;
+            case ROOK:
+                validMoves = bb.getRookMoves(from, or);
+                break;
+            case BISHOP:
+                validMoves = bb.getBishopMoves(from, or);
+                break;
+            case KNIGHT:
+                validMoves = bb.getKnightMoves(from, or);
+                break;
+            case QUEEN:
+                validMoves = bb.getQueenMoves(from, or);
+                break;
+            case KING:
+                validMoves = bb.getKingMoves(from, or);
+                break;
+            default:
+                break;
+        }
+
+       if(or == Orientation.BLACK) {
+           if(bb.playForBlack(validMoves, from, to) == -1)
+               return -1;
+       } 
+
+       if(bb.playForWhite(validMoves, from, to) == -1)
+           return -1;
+
+       p.updatePiece(to);
+       return 0;
     }
 
+
+
     public static void main(String[] args) {
-
-        Player p1 = new Player(Orientation.WHITE);
-        Player p2 = new Player(Orientation.BLACK);
-
-        p1.printPlayerData();
-        System.out.println("\n\n");
-        p2.printPlayerData();
+        System.out.println("in main() for Player.java");
     }
 }
