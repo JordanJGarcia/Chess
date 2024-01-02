0c61eff81bc6541cbfb1de1747d2b76dc1edb2be
Jordan Garcia <j.joseph_g@yahoo.com>
2024-01-02 11:01:15 -0500

    add ability to track and switch the current player

--- Board.java
+++ Board.java
@@ -8,20 +8,22 @@ class Board {
     // the idea is for Board to be the 2nd tier encompassing class
     //      - A board will have an underlying bitboard, shared by 2 players, each player has 16 pieces, etc.
     //      - a GUI will have an underlying board that it sends requests to and portrays on the screen
-    private Player p1, p2;
+    private Player p1, p2, currentPlayer;
     private BitBoard bb;
 
     // default constructor
     Board() {
         bb = new BitBoard();
-        p1 = new Player(WHITE, bb);
-        p2 = new Player(BLACK, bb);
+        setPlayerOne(new Player(WHITE, bb));
+        setPlayerTwo(new Player(BLACK, bb));
+        setCurrentPlayer(getPlayerOne());
     }
 
     // constructor
     Board(Player p1, Player p2, BitBoard bb) {
         setPlayerOne(p1);
         setPlayerTwo(p2);
+        setCurrentPlayer(p1);
         setBitBoard(bb);
     }
 
@@ -41,6 +43,10 @@ class Board {
         p2 = p;
     }
 
+    void setCurrentPlayer(Player p) {
+        currentPlayer = p;
+    }
+
     Player getPlayerOne() {
         return p1;
     }
@@ -49,14 +55,25 @@ class Board {
         return p2;
     }
 
-    void getBitBoard() {
-        bb.printBitBoard();
+    Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    BitBoard getBitBoard() {
+        return bb;
     }
 
     void setBitBoard(BitBoard b) {
         bb = b;
     }
     
+    void switchPlayers() {
+        if(getCurrentPlayer() == getPlayerOne())
+            setCurrentPlayer(getPlayerTwo());
+        else
+            setCurrentPlayer(getPlayerOne());
+    }
+
     public static void main(String[] args) {
         Board board = new Board();
         board.printPlayerData();
